cb88c8db75f63be9d68bc1d874fa937fd826b49c
asw357 <wilcoboesveld12@gmail.com>
2025-08-09 19:36:40 +0200

    Update logout.js

--- logout.js
+++ logout.js
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="nl">
 <head>
-  <mta charset="utf-8" />
+  <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width,initial-scale=1" />
   <title>Uitloggen</title>
   <script src="https://cdn.tailwindcss.com"></script>
