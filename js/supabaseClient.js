6931488917dca6acc8924b858740a77e38903fa1
asw357 <wilcoboesveld12@gmail.com>
2025-08-09 19:45:04 +0200

    Create supabaseClient.js

---
+++ js/supabaseClient.js
@@ -0,0 +1,9 @@
+<script type="module">
+// Re-usable Supabase client
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+
+export const supabase = createClient(
+  'https://yqbejgnhmizkeuxxpbbb.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlxYmVqZ25obWl6a2V1eHhwYmJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ1Nzg2NDksImV4cCI6MjA3MDE1NDY0OX0.L_v6XgPh7F6KrWKlu72f6jbL_QTU-hdMQa0lA-QFryM'
+);
+</script>
