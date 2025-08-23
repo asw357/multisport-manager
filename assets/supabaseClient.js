a17b45a416723e60e4981f9d28fbb4e890419097
asw357 <wilcoboesveld12@gmail.com>
2025-08-23 10:09:53 +0200

    Update supabaseClient.js

--- assets/supabaseClient.js
+++ assets/supabaseClient.js
@@ -1,4 +1,3 @@
-<script type="module">
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 import { SUPABASE_URL, SUPABASE_ANON_KEY } from "./config.js";
 
@@ -9,4 +8,3 @@ export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
     detectSessionInUrl: true,
   },
 });
-</script>
